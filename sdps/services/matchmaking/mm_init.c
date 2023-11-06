eb41a5ada6caa6efb2199901f868023b89364cab
noahw2021 <93048049+noahw2021@users.noreply.github.com>
2023-11-06 09:02:48 -0500

    mm system cleanup, free memory

--- sdps/services/matchmaking/mm_init.c
+++ sdps/services/matchmaking/mm_init.c
@@ -11,9 +11,41 @@ OSU Software Design Project Game Server
 PMATCHMAKING_CLIENT MmClient;
 
 VOID MmInitSystems(VOID) {
+    MmClient = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY,
+        sizeof(MATCHMAKING_CLIENT));
 
+    MmcUpdateGameServerInfo();
+    return;
 }
 
 VOID MmShutdownSystem(VOID) {
+    if (!MmClient)
+        return;
 
+    if (MmClient->Lobbies) {
+        for (int i = 0; i < MmClient->LobbyCount) {
+            PMATCHMAKING_LOBBY ThisLobby = &MmClient->Lobbies[i];
+
+            if (ThisLobby->Users)
+                HeapFree(GetProcessHeap(), NULL, ThisLobby->Users);
+        }
+
+        HeapFree(GetProcessHeap(), NULL, MmClient->Lobbies); 
+    }
+
+    if (MmClient->Users)
+        HeapFree(GetProcessHeap(), NULL, MmClient->Users);
+
+    if (MmClient->Servers) {
+        for (int i = 0; i < MmClient->ServerCount; i++) {
+            PMATCHMAKING_SERVER ThisServer = &MmClient->Servers[i];
+
+            if (ThisServer->MmUsers)
+                HeapFree(GetProcessHeap(), NULL, ThisServer->MmUsers);
+        }
+        HeapFree(GetProcessHeap(), NULL, MmClient->Servers);
+    }
+
+    HeapFree(GetProcessHeap(), NULL, MmClient);
+    return;
 }
