ce1cf7bd1beeaff0effe44e01f16e3c4231d23fe
noahw2021 <93048049+noahw2021@users.noreply.github.com>
2023-11-06 08:56:06 -0500

    Create mm_communicate.c

---
+++ sdps/services/matchmaking/mm_communicate.c
@@ -0,0 +1,12 @@
+/*
+mm_communicate.c
+OSU Software Design Project -- Game Server / Game Coordinator
+sdps
+
+(c) Noah Wooten 2023, All Rights Reserved
+OSU Software Design Project Game Server
+*/
+
+VOID MmcUpdateGameServerInfo(VOID) {
+  return;
+}
